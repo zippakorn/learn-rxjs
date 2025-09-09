69ac10ed68ef0b0406ed2d840883446bbdde0643
Sippakorn Phuakpong <sphuakpong@gmail.com>
2025-09-09 16:49:26 +0700

    feat: init case without rxjs and properly tool design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import atomOneDark from "svelte-highlight/styles/atom-one-dark";
+	import mermaid from 'mermaid';
 
 	let { children } = $props();
+	mermaid.initialize({ startOnLoad: true, theme: 'dark' });
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	{@html atomOneDark}
 </svelte:head>
 
-{@render children?.()}
+<div class="flex h-screen">
+	<aside class="w-64 bg-gray-900 text-gray-200 p-4 shadow-lg">
+			<h1 class="text-xl font-bold text-white mb-4">Learn RxJS</h1>
+			<nav>
+					<ul class="space-y-2">
+							<li><a href="/" class="text-gray-400 hover:text-white">Home</a></li>
+							<li><a href="/intro" class="text-gray-400 hover:text-white">Intro</a></li>
+							<li><a href="/contact" class="text-gray-400 hover:text-white">Contact</a></li>
+					</ul>
+			</nav>
+	</aside>
+	<main class="flex-1 p-4 overflow-y-auto bg-gray-800 text-gray-100">
+			{@render children?.()}
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,121 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Highlight, { LineNumbers } from 'svelte-highlight';
+	import typescript from 'svelte-highlight/languages/typescript';
+	import mermaid from 'mermaid';
+	import { onMount } from 'svelte';
+
+  import * as Tabs from "$lib/components/ui/tabs";
+
+	$: sample1 = /*ts*/ `
+    const customer = new Customer();
+    const chef = new Chef();
+
+    const requesRiceFromRiceFarmer = Promise.reslove("rice");
+
+    chef.on('serve', dish => {
+      customer.eat(dish);
+    });
+    chef.on('notify', message => {
+      customer.complain(message);
+    });
+
+    requesRiceFromRiceFarmer.then(rice => {
+      const friedRice = chef.cook(rice);
+      chef.serve(friedRice);
+      chef.close();
+    }).catch(err => {
+      chef.notify("Sorry, we are out of rice today");
+      chef.close();
+    })
+  `;
+
+  $: sample2 = /*ts*/ `
+    const chef = new Chef();
+    const refrigerator = new Refrigerator();
+    const requestCompletion = {
+      rice: false,
+      tuna: false
+    }
+
+    const requesRiceFromRiceFarmer = Promise.reslove("rice");
+    const requestTunaFromFisherMan = Promise.reslove("tuna");
+
+    chef.on('serve', dish => {
+      customer.eat(dish);
+    });
+    chef.on('notify', message => {
+      customer.complain(message);
+    });
+
+    requesRiceFromRiceFarmer.then(rice => {
+      const friedRice = chef.cook(rice);
+      chef.serve(friedRice);
+
+      if (refrigerator.has("tuna")) {
+        const tuna = refrigerator.get("tuna");
+        const sushi = chef.cook(tuna, rice);
+        chef.serve(sushi);
+      } else {
+        refrigerator.add("rice", rice);
+      }
+
+      requestCompletion.rice = true;
+
+      if (requestCompletion.tuna === true) {
+        chef.close();
+      }
+    }).catch(err => {
+      chef.notify("Sorry, we are out of rice today");
+      requestCompletion.rice = true;
+
+      if (requestCompletion.tuna === true) {
+        chef.close();
+      }
+    })
+
+    requestTunaFromFisherMan.then(tuna => {
+      if (!refrigerator.has("rice")) {
+        refrigerator.add("tuna", tuna);
+        return
+      }
+
+      const rice = refrigerator.get("rice");
+      const sushi = chef.cook(tuna, rice);
+      chef.serve(sushi);
+      requestCompletion.tuna = true;
+
+      if (requestCompletion.rice === true) {
+        chef.close();
+      }
+    }).catch(err => {
+      chef.notify("Sorry, we are out of suhi today");
+      requestCompletion.tuna = true;
+
+      if (requestCompletion.rice === true) {
+        chef.close();
+      }
+    })
+  `;
+
+	onMount(async () => {
+		mermaid.contentLoaded();
+	});
+
+  $: step = '1';
+</script>
+<Tabs.Root bind:value={step} class="w-full">
+  <Tabs.List>
+    <Tabs.Trigger value="1">1 Dependency</Tabs.Trigger>
+    <Tabs.Trigger value="2">2 Dependencies</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="1">
+    <Highlight language={typescript} code={sample1} let:highlighted>
+      <LineNumbers {highlighted} hideBorder />
+    </Highlight>
+  </Tabs.Content>
+  <Tabs.Content value="2">
+    <Highlight language={typescript} code={sample2} let:highlighted>
+      <LineNumbers {highlighted} hideBorder />
+    </Highlight>
+  </Tabs.Content>
+</Tabs.Root>
